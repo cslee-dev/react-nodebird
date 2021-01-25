593be31b7150c7da32f407d8279b3fa394b6318e
lcs <cslee@itmoulds.com>
2021-01-25 21:06:51 +0900

    프로필/회원가입 헤드 적용하기

--- pages/profile.js
+++ pages/profile.js
@@ -1,8 +1,12 @@
 import AppLayout from "../components/AppLayout";
+import Head from "next/head";
 
 function Profile() {
   return (
     <AppLayout>
+      <Head>
+        <title>프로필</title>
+      </Head>
       <div>Profile</div>
     </AppLayout>
   )
--- pages/signup.js
+++ pages/signup.js
@@ -1,8 +1,12 @@
 import AppLayout from "../components/AppLayout";
+import {Head} from "next/document";
 
 function Signup() {
   return (
     <AppLayout>
+      <Head>
+        <title>회원가입</title>
+      </Head>
       <div>Sign up</div>
     </AppLayout>
   )
